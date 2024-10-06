0f0e9324435ae975e6f1c3fd77b5a4eefe19d013
Yash <sahib1701@gmail.com>
2024-10-06 10:42:02 -0400

    Initial commit

---
+++ backend/src/test/java/com/financeApp/AmortizedAnalysis/AmortizedAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financeApp.AmortizedAnalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmortizedAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
